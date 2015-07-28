77c1edee8afac7b90039ac3db5721e9c770f9413
nokiddin <nokiddin-gl@yahoo.com>
2015-07-28 12:25:25 +0530

    CHG: Compat with eta projects until patch is accepted in ampq-coffee

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,12 +3,16 @@ SCRIPT_PATH=`dirname $0`
 echo "amqp-coffee Compiling coffeescript to bin/"
 echo $SCRIPT_PATH
 
+# id;
+
 rm -rf $SCRIPT_PATH/../bin
 mkdir $SCRIPT_PATH/../bin
 
 cp -r $SCRIPT_PATH/../src $SCRIPT_PATH/../bin/
 
+export PATH=$SCRIPT_PATH/../node_modules/coffee-script/bin:$PATH;
+
 # compile all coffeescript files
-find $SCRIPT_PATH/../bin -name "*.coffee" | xargs $SCRIPT_PATH/../node_modules/coffee-script/bin/coffee --compile
+find $SCRIPT_PATH/../bin -name "*.coffee" | xargs coffee --compile
 # remove all coffeescript files
 find $SCRIPT_PATH/../bin -name "*.coffee" | xargs rm
